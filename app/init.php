3104c2ba5e415d782588ec4716cfb5003f48951e
anindyo <anindyo0216@gmail.com>
2023-05-01 20:49:57 +0700

    docs: add init.php

---
+++ app/init.php
@@ -0,0 +1,12 @@
+<?php 
+
+// CORE
+require_once('core/App.php');
+require_once('core/Controller.php');
+require_once('core/Database.php');
+require_once('core/Flasher.php');
+
+// CONFIG
+require_once('config/config.php');
+
+?>
