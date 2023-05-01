0b17536a87d39b0b736359d413798bfc45cbef00
anindyo <anindyo0216@gmail.com>
2023-05-01 20:37:30 +0700

    docs: add config.php

---
+++ app/config/config.php
@@ -0,0 +1,12 @@
+<?php
+
+// URL
+define('BASEURL', 'http://localhost/dermanifest_mvc/public'); 
+
+// DATABASE
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'dermanifest_db');
+
+?>
