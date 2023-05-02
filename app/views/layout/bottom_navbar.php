d7f15c51ae3f9edd79ec33fc79bea01768a81b15
sanisapascaline <sanisapascaline11@gmail.com>
2023-05-02 21:44:07 +0700

    docs: add bottom_navbar.php

---
+++ app/views/layout/bottom_navbar.php
@@ -0,0 +1,59 @@
+<section class="sec-footer" id="scroll-footer">
+  <div class="container container-footer">
+    <div class="row row-footer">
+      <div class="col-lg-12 col-md-12 col-sm-12 col-footer">
+        <div class="footer-logo">
+          <img src="#" class="img-footer mb-4" alt="">
+          <p class="text-footer text-center mb-5">
+            Thank you for trusting Demanifest as your skincare choice. We will always provide the best service for your skin health, no matter the age, skin, or anything else.
+          </p>
+        </div>
+        <div class="contact-footer">
+          <div class="footer-info">
+              <h5>Contact</h5>
+              <ul class="footer-items">
+                <li class="item">Phone : <a href="#"><u>Chat on Whatsapp</u></a></li>
+                <li class="item">Mail : dermanifest@gmail.com </li>
+              </ul>
+          </div>
+          <div class="footer-info">
+              <h5>Media Social</h5>
+              <ul class="footer-items">
+                <li class="item">Facebook</li>
+                <li class="item"><a href="https://www.instagram.com/dermanifest">Instagram <i class="fa fa-external-link" aria-hidden="true"></i></a></li>
+                <li class="item"><a href="https://www.tiktok.com/@dermanifest">Tiktok <i class="fa fa-external-link" aria-hidden="true"></i></a></li>
+                <li class="item">Twitter</li>
+              </ul>
+          </div>
+          <div class="footer-info">
+              <h5>Our Store</h5>
+              <ul class="footer-items">
+                <li class="item">Tokopedia</li>
+                <li class="item">Shopee</li>
+                <li class="item">Bukalapak</li>
+              </ul>
+          </div>
+          <div class="footer-info">
+              <h5>Address</h5>
+              <ul class="footer-items">
+                <li class="item">Kelapa Gading, Jakarta Utara</li>
+                <li class="item">Rawamangun, Jakarta Timur</li>
+                <li class="item">Ciracas, Jakarta Timur</li>
+              </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+<!-- term condition -->
+<section class="term">
+  <div class="container container-term">
+    <div class="row row-term">
+      <div class="col-lg-12 col-md-12 col-sm-12 col-term">
+        <p>© 2023 Dermanifest | All rights reserved.</p>
+      </div>
+    </div>
+  </div>
+</section>
