78c8b6915d326ce0f3bf20e1180e9d2b62f2a6c0
sanisapascaline <sanisapascaline11@gmail.com>
2023-05-02 21:42:15 +0700

    feat: add codes for home contents

---
+++ app/views/home/index.php
@@ -0,0 +1,211 @@
+<!-- HERO -->
+<section class="hero">
+  <div class="container hero-container">
+    <div class="row hero-row">
+      <div class="col-lg-12 col-md-12 col-sm-12">
+        <div class="hero-col">
+          <div class="text-hero">
+            <h2>Hello, Customer Name</h2>
+            <p>
+              Take a litte journey today</br>
+              and search anything you want.
+            </p>
+            <div>
+              <a href="<?= BASEURL;?>/login" class="btn btn-primary-native">Log In</a>
+              <a href="<?= BASEURL;?>/register" class="btn btn-secondary-native secondary-color">Register</a>
+            </div>          
+          </div>
+          
+          <div id="carouselExampleIndicators" class="image-hero carousel slide" data-bs-ride="carousel" data-bs-touch="true">
+            <div class="carousel-indicators">
+              <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+              <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+              <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+              <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="3" aria-label="Slide 4"></button>
+              <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="4" aria-label="Slide 5"></button>
+              <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="5" aria-label="Slide 6"></button>
+            </div>
+            <div class="carousel-inner">
+              <div class="carousel-item active">
+                <img src="#" class="d-block w-100" alt="...">
+              </div>
+              <div class="carousel-item">
+                <img src="#" class="d-block w-100" alt="...">
+              </div>
+              <div class="carousel-item">
+                <img src="#" class="d-block w-100" alt="...">
+              </div>
+              <div class="carousel-item">
+                <img src="#" class="d-block w-100" alt="...">
+              </div>
+              <div class="carousel-item">
+                <img src="#" class="d-block w-100" alt="...">
+              </div>
+              <div class="carousel-item">
+                <img src="#" class="d-block w-100" alt="...">
+              </div>
+            </div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+              <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+              <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+              <span class="carousel-control-next-icon" aria-hidden="true"></span>
+              <span class="visually-hidden">Next</span>
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+<!-- SPACER -->
+<section class="spacer">
+  <div class="container spacer-container">
+    <div class="row spacer-row">
+      <div class="col-lg-2 col-md-2 col-sm-4 col-4 col-item mb-3 p-5">
+              
+      </div>
+    </div>
+  </div>
+</section>
+
+<!-- ABOUT -->
+<section class="about" id="scroll-about">
+  <div class="container about-container">
+    <div class="row about-row">
+      <div class="col-lg-8 col-md-6 col-sm-12">
+        <div class="title mb-3">
+          <h1>About Us</h1>
+        </div>
+        <div class="paragpraph mb-5">
+          <p>Dermanifest is a skincare product made from natural and cruelty ingredients,
+              made with love for everyone with any skin and any ages.
+          </p>
+        </div>
+        <div class="subtitle mb-3">
+          <h5>What do we have for you?</h5>
+        </div>
+        <div class="item-product mb-5">
+          <ul class="items">
+            <li class="item">Dermanifest Classic Powder Beauty Mask</li>
+            <li class="item">Dermanifest Classic Candlenut Oil</li>
+            <li class="item">Dermanifest Classic Scented Candle</li>
+            <li class="item">Featured Skincare</li>
+          </ul>
+        </div>
+      </div>
+
+      <div class="col-lg-4 col-md-6 col-sm-12">
+        <div class="vmage-about">
+          <img src="#" alt="">
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+<!-- PRODUCT -->
+<section class="product">
+  <div class="container container-product swiper productSwiper">
+    <div class="row row-title-product mb-5">
+      <div class="col-lg-12 col-md-12 col-sm-12">
+        <div class="product-content">
+          <div class="title">
+            <h1>Top Product</h1>
+          </div>
+          <div class="paragraph">
+            <p>Our top product, truthfully crafted for you.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="row row-product swiper-wrapper mb-5" id="scrollhere">
+      <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 swiper-slide">
+        <a href="<?= BASEURL; ?>/product/idnyadisini">
+          <div class="card swiper-slide">
+            <div class="image-product">
+              <div class="hover-desc">
+                <p><i class="fa fa-search" aria-hidden="true"></i></p>
+              </div>
+              <img src="#" alt="...">
+            </div>
+
+            <div class="product-content">
+              <div class="text-content">
+                <h5>Nama Produk</h5>
+                <p>Kategori</p>
+              </div>
+
+              <div class="price-rate">
+                <div class="product-rate">
+                    <i class='bx bxs-star'></i>
+                    <p>4.5</p>
+                </div>
+                <!--p class="disc-price" >Rp.350.000</p-->
+                <div class="product-price">
+                  <p>Rp30.000,-</p>
+                </div>
+              </div>
+
+              <a href="<?= BASEURL; ?>/product/idnyadisini" class="btn btn-buy btn-cart">
+                  <box-icon type='solid' name='cart-add'><i class='bx bxs-cart-add' style="width: 20px; height: auto;"></i></box-icon> Add to Cart 
+              </a>
+            </div>
+          </div>
+        </a>
+      </div>
+    </div> 
+    <div class="row">
+      <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 d-flex col-button">
+        <p class="button-prev me-3">PREV</p>
+        <p class="button-next">NEXT</p>
+      </div>
+    </div>
+  </div>
+</section> 
+
+<!-- BUY -->
+<section>
+  <div class="container buy-container">
+    <div class="row buy-row">
+      <div class="col-lg-6 col-md-6 col-sm-12 col-12 col-buy">
+        <div class="title mb-3">
+          <h1>Buy Here</h1>
+        </div>
+        <div class="paragraph mb-5">
+          <p>You can purchase our products comfortably and 
+              safely through the following media or
+              <span class="link-contact">
+                <a href="#" target="_blank">Whatsapp us for fast service</a>
+              </span>
+            </p>
+        </div>
+        <div class="button-buy">
+          <a href="<?= BASEURL; ?>/product/index" class="btn btn-buy">Shop Now</a>
+        </div>
+      </div>
+      <div class="col-lg-6 col-md-6 col-sm-12 col-12 col-img">
+          <img src="#" alt="">
+      </div>
+    </div>
+
+    <div class="row eshop-row">
+      <div class="col-lg-12 col-md-12 col-sm-12 col-12 col-eshop">
+        <div class="image-item">
+          <img src="#" alt="">
+        </div>
+        <div class="image-item">
+          <img src="#" alt="">
+        </div>
+        <div class="image-item">
+          <img src="#" alt="">
+        </div>
+        <div class="image-item">
+          <img src="#" alt="">
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
