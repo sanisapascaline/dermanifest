54f30751dfb54ad9cfb0dcd6850c91e8c58abaf8
sanisapascaline <sanisapascaline11@gmail.com>
2023-05-02 21:44:24 +0700

    docs: add navbar.php

---
+++ app/views/layout/navbar.php
@@ -0,0 +1,55 @@
+<section class="navbar-fixed-top">
+  <div class="container py-3">
+    <nav class="navbar navbar-expand-lg navbar-light nav-dermanifest">
+      <div class="container-fluid">
+        <a class="navbar-brand nav-logo" href="#"><img src="#" alt="" srcset=""></a>
+      </div>
+
+      <button class="navbar-toggler nav-toggle" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <i class='bx bx-dots-horizontal-rounded nav-mobile-menu' ></i>
+      </button>
+
+      <li class="navbar-toggler nav-toggle">
+        <a class="n" href="#"><i class='bx bx-cart'></i></a>
+      </li>
+      <div class="collapse navbar-collapse dorpdown-mobile" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item me-4">
+            <a class="nav-link" href="#">Home</a>
+          </li>
+          <li class="nav-item me-4">
+            <a class="nav-link" href="index.php#scroll-about">About</a>
+          </li>
+          <li class="nav-item me-4 dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                Shop
+            </a>
+            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+              <li><a class="dropdown-item" href="#">All Product</a></li>
+              <li><a class="dropdown-item" href="#">Dermanifest Classic Powder Beauty Mask</a></li>
+              <li><a class="dropdown-item" href="#">Dermanifest Classic Candlenut Oil</a></li>
+              <li><a class="dropdown-item" href="#">Dermanifest Classic Scented Candle</a></li>
+              <li><a class="dropdown-item" href="#">Featured Skincare</a></li>
+            </ul>
+          </li>
+          <li class="nav-item me-4">
+            <a class="nav-link" href="index.php#scroll-footer">Contact</a>
+          </li>
+          
+          <div class="nav-ico-mobile">
+            <li class="nav-item me-4 cart-nav hide-med">
+              <a class="nav-link" href="#"><i class='bx bx-cart'></i></a>
+              <span class='badge badge-warning' id='lblCartCount'>
+                0                    
+              </span>
+            </li>
+            <li class="nav-item me-4">
+              <a class="nav-link" href="#"><i class='bx bx-user'></i></a>
+            </li>
+          </div>
+        </ul>
+      </div>
+    </nav>
+  </div>
+</section>
+<div class="whitespace"></div>
