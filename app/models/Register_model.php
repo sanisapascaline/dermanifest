dc187bd35aabb5c3811474f43bead5c651c6b331
anindyoo <anindyo0216@gmail.com>
2023-05-05 20:37:03 +0700

    feat: add customer register query

---
+++ app/models/Register_model.php
@@ -0,0 +1,43 @@
+<?php
+
+class Register_model {
+  private $table = 'customer';
+	private $db;
+
+	public function __construct()
+	{
+		$this->db = new Database;
+	}
+
+  public function checkUsername($username) {
+    $this->db->query("SELECT * FROM " . $this->table . " WHERE username = :username");
+    $this->db->bind('username', $username);
+
+    return $this->db->single();
+  }
+  
+  public function checkAdminUsername($username) {
+    $this->db->query("SELECT * FROM admin WHERE username = :username");
+    $this->db->bind('username', $username);
+
+    return $this->db->single();
+  }
+
+  public function doRegister($data) {
+    $query = "INSERT INTO " . $this->table . " 
+              (name, username, email, phone, password)
+                VALUES
+              (:name, :username, :email, :phone, :password)";
+    $this->db->query($query);
+    $this->db->bind('name', $data['name']);
+    $this->db->bind('username', $data['username']);
+    $this->db->bind('email', $data['email']);
+    $this->db->bind('phone', $data['phone']);
+    $this->db->bind('password', md5($data['password']));
+
+    $this->db->execute();
+
+    return $this->db->rowCount();
+  }
+}
+?>
