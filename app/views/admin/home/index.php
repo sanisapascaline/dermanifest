134660b9e01ccfcfefee404e1a2ad4b9197ecba7
sanisapascaline <58965231+sanisapascaline@users.noreply.github.com>
2023-05-07 23:47:22 +0800

    feat: add information cards on admin home

--- app/views/admin/home/index.php
+++ app/views/admin/home/index.php
@@ -1,4 +1,36 @@
 <div class="content">
+  <h2>Categories</h2>
+  <hr>
+  <div class="home-cards">
+    <div class="card">
+      <div class="card-icon"><i class="fa-solid fa-users"></i></div>
+      <div class="card-desc">
+        <div class="card-value">20</div>
+        <div class="card-value-desc">Customers</div>
+      </div>
+    </div>
+    <div class="card">
+      <div class="card-icon"><i class="fa-solid fa-cart-shopping"></i></div>
+      <div class="card-desc">
+        <div class="card-value">14</div>
+        <div class="card-value-desc">Overrall Orders</div>
+      </div>
+    </div>
+    <div class="card">
+      <div class="card-icon"><i class="fa-solid fa-clock"></i></div>
+      <div class="card-desc">
+        <div class="card-value">2</div>
+        <div class="card-value-desc">Waiting Orders</div>
+      </div>
+    </div>
+    <div class="card">
+      <div class="card-icon"><i class="fa-solid fa-money-bill-trend-up"></i></div>
+      <div class="card-desc">
+        <div class="card-value">Rp1.000.000,-</div>
+        <div class="card-value-desc">Gross Sales</div>
+      </div>
+    </div>
+  </div>
   <p>
     Welcome to Admin Page, <strong> <?= $_SESSION['admin']['name']; ?> </strong>
   </p>
