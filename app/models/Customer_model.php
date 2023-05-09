89336348e4157d133af035351b0ca6c0d82b2138
anindyoo <anindyo0216@gmail.com>
2023-05-09 23:01:56 +0700

    feat: add profile update queries

--- app/models/Customer_model.php
+++ app/models/Customer_model.php
@@ -16,5 +16,39 @@ class Customer_model {
     
     return $this->db->single();
   }
+
+  public function updateDataCustomer($data) 
+  {
+    $query = "UPDATE " . $this->table . " 
+              SET name = :name,
+              username = :username,
+              email = :email,
+              phone = :phone
+              WHERE id_customer = :id_customer";
+		$this->db->query($query);
+		$this->db->bind('name', $data['name']);
+		$this->db->bind('username',$data['username']);
+		$this->db->bind('email',$data['email']);
+		$this->db->bind('phone',$data['phone']);
+		$this->db->bind('id_customer', $data['id_customer']);
+		
+    $this->db->execute();
+
+		return $this->db->rowCount();
+  }
+
+  public function updateDataPassword($data)
+  {
+    $query = "UPDATE " .$this->table . " 
+              SET password = :password
+              WHERE id_customer = :id_customer";
+    $this->db->query($query);
+    $this->db->bind('password', md5($data['new_password']));
+		$this->db->bind('id_customer', $data['id_customer']);
+
+    $this->db->execute();
+
+		return $this->db->rowCount();
+  }
 }
 ?>
