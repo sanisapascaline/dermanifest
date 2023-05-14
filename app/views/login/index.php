30028af513ac6855f7028f84c198b2855ebe04a3
sanisapascaline <58965231+sanisapascaline@users.noreply.github.com>
2023-05-14 12:59:54 +0800

    fix: typo on google button

--- app/views/login/index.php
+++ app/views/login/index.php
@@ -32,7 +32,7 @@
 
           <div>
             <a class="btn btn-outline-dark btn-login" href="<?= BASEURL; ?>/google_auth" role="button" style="text-transform:none; background-color: white; width: 100%">
-              <img width="20px" style="width: 20px !important; margin-bottom:3px; margin-right:5px;" alt="Google sign-in" src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/512px-Google_%22G%22_Logo.svg.png" /> Login with Google
+              <img width="20px" style="width: 20px !important; margin-bottom:3px; margin-right:5px;" alt="Google sign-in" src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/512px-Google_%22G%22_Logo.svg.png" /> Log In with Google
             </a>
           </div>
         </div>
