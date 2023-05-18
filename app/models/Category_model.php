d59c1fc1d039a0d35f1fa3fce1defde575dbc758
anindyoo <anindyo0216@gmail.com>
2023-05-18 15:07:17 +0700

    feat: add method for getting category by id

--- app/models/Category_model.php
+++ app/models/Category_model.php
@@ -16,6 +16,14 @@ class Category_model {
     return $this->db->resultSet();
   }
 
+  public function getCategoryById($id)
+  {
+    $this->db->query("SELECT * FROM " . $this->table . " WHERE id_category = :id_category");
+    $this->db->bind('id_category', $id);
+
+    return $this->db->single();
+  }
+
   public function insertDataCategory($data) 
   {
     $query = "INSERT INTO " . $this->table . " (name) VALUES (:name)";
