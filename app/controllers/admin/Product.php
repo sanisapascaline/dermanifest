dbd91a1f1fa48a6160b1913a2941dd1f58609355
anindyoo <anindyo0216@gmail.com>
2023-05-20 23:13:17 +0700

    feat: add products data into admin products view

--- app/controllers/admin/Product.php
+++ app/controllers/admin/Product.php
@@ -16,7 +16,15 @@ class Product extends Controller {
   public function index() 
   {
     $data['judul'] = 'Product | Dermanifest Admin';
-    
+    $data['product_list'] = $this->model('Product_model')->getAllProduct();
+    $data['category_list'] = $this->model('Category_model')->getAllCategory();
+    $product_category_names = [];
+
+    foreach ($data['category_list'] as $category) {
+      $product_category_names[$category['id_category']] = $category['name'];
+    }
+  
+    $data['category_names'] = $product_category_names;
     $this->view('layout/admin/header', $data);
     $this->view('layout/admin/sidebar');
     $this->viewAdmin('product/index', $data);
