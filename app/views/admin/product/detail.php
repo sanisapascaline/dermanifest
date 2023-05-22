6ef6ba5ba24dfd86c3b7e2eb11694a29d23979a7
sanisapascaline <58965231+sanisapascaline@users.noreply.github.com>
2023-05-22 20:35:10 +0800

    feat: add UI code for admin detail product

---
+++ app/views/admin/product/detail.php
@@ -0,0 +1,58 @@
+<div class="content">
+  <h2>Product Detail</h2>
+  <hr>
+  <div>
+    <?php Flasher::flash(); ?>
+  </div>
+  <table class="table table-bordered table-striped table-hover table-header-on-left">
+    <tbody>
+      <tr>
+        <th>Id</th>
+        <td>id product</td>
+      </tr>
+      <tr>
+        <th>Name</th>
+        <td>product name</td>
+      </tr>
+      <tr>
+        <th>Category</th>
+        <td>product category</td>
+      </tr>
+      <tr>
+        <th>Price</th>
+        <td>30000</td>
+      </tr>
+      <tr>
+        <th>Stock</th>
+        <td>20</td>
+      </tr>
+      <tr>
+        <th>Neto</th>
+        <td>100 gr</td>
+      </tr>
+      <tr>
+        <th>Gross Weight</th>
+        <td>200 gr</td>
+      </tr>
+      <tr>
+        <th>Description</th>
+        <td>product description</td>
+      </tr>
+      <tr>
+        <th>Instruction</th>
+        <td>product instruction</td>
+      </tr>
+      <tr>
+        <th>Ingredients</th>
+        <td>product ingredients</td>
+      </tr>
+    </tbody>
+  </table>
+
+  <h3 class="mt-20px">Pictures</h3>
+  <div class="row">
+    <div class="col-md-3">
+  		<img src="#"> <br>
+    </div>
+  </div>
+</div>
