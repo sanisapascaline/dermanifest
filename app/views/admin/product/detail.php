f30a18c52d04debf94b67eb79f1e73dc580505b6
sanisapascaline <58965231+sanisapascaline@users.noreply.github.com>
2023-05-22 22:37:15 +0800

    feat: add product detail data from database

--- app/views/admin/product/detail.php
+++ app/views/admin/product/detail.php
@@ -8,51 +8,53 @@
     <tbody>
       <tr>
         <th>Id</th>
-        <td>id product</td>
+        <td><?= $data['product']['id_product'] ?></td>
       </tr>
       <tr>
         <th>Name</th>
-        <td>product name</td>
+        <td><?= $data['product']['product_name'] ?></td>
       </tr>
       <tr>
         <th>Category</th>
-        <td>product category</td>
+        <td><?= $data['product']['category_name'] ?></td>
       </tr>
       <tr>
         <th>Price</th>
-        <td>30000</td>
+        <td><?= $data['product']['price'] ?></td>
       </tr>
       <tr>
         <th>Stock</th>
-        <td>20</td>
+        <td><?= $data['product']['stock'] ?></td>
       </tr>
       <tr>
         <th>Neto</th>
-        <td>100 gr</td>
+        <td><?= $data['product']['neto'] ?> gr</td>
       </tr>
       <tr>
         <th>Gross Weight</th>
-        <td>200 gr</td>
+        <td><?= $data['product']['gross_weight'] ?></td>
       </tr>
       <tr>
         <th>Description</th>
-        <td>product description</td>
+        <td><?= $data['product']['description'] ?></td>
       </tr>
       <tr>
         <th>Instruction</th>
-        <td>product instruction</td>
+        <td><?= $data['product']['instruction'] ?></td>
       </tr>
       <tr>
         <th>Ingredients</th>
-        <td>product ingredients</td>
+        <td><?= $data['product']['ingredients'] ?></td>
       </tr>
     </tbody>
   </table>
 
   <h3 class="mt-20px">Pictures</h3>
   <div class="row">
+    <?php foreach ($data['product_picture_list'] as $picture) :?>
     <div class="col-md-3">
-  		<img src="#"> <br>
+  		<img src="<?= IMGURL; ?>/products/<?= $picture['picture_name']; ?>" width="80%" height="auto"> <br>
     </div>
+    <?php endforeach;?>
   </div>
 </div>
