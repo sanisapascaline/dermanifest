aa8a93c1bd70839c1885bf2a44a5871cce39dbef
anindyoo <anindyo0216@gmail.com>
2023-05-25 16:13:50 +0700

    feat: add query for updating product data

--- app/models/Product_model.php
+++ app/models/Product_model.php
@@ -38,6 +38,7 @@ class Product_model {
 						 VALUES 
 						 (:id_category, :name, :price, :main_picture, :stock, :neto, :description, :instruction, :ingredients, :gross_weight)";
 		$this->db->query($query);
+    
     $this->db->bind('id_category', $data['id_category']);
     $this->db->bind('name', $data['name']);
     $this->db->bind('price', $data['price']);
@@ -53,5 +54,38 @@ class Product_model {
 
     return $this->db->rowCount();
 	}
+
+  public function updateDataProduct($data, $picture)
+  {
+    $query = "UPDATE " . $this->table . " 
+              SET id_category = :id_category,
+              name = :name,
+              price = :price,
+              main_picture = :main_picture,
+              stock = :stock,
+              neto = :neto,
+              description = :description,
+              instruction = :instruction,
+              ingredients = :ingredients,
+              gross_weight = :gross_weight
+              WHERE id_product = :id_product";
+		$this->db->query($query);
+
+		$this->db->bind('id_category', $data['id_category']);
+    $this->db->bind('name', $data['name']);
+    $this->db->bind('price', $data['price']);
+    $this->db->bind('main_picture', $picture);
+    $this->db->bind('stock', $data['stock']);
+    $this->db->bind('neto', $data['neto']);
+    $this->db->bind('description', $data['description']);
+    $this->db->bind('instruction', $data['instruction']);
+    $this->db->bind('ingredients', $data['ingredients']);
+    $this->db->bind('gross_weight', $data['gross_weight']);
+		$this->db->bind('id_product', $data['id_product']);
+		
+    $this->db->execute();
+
+		return $this->db->rowCount();
+  }
 }
 ?>
