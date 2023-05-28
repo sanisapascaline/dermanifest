fe764657ace439eeff8ffd3d11d4129f75552896
anindyoo <anindyo0216@gmail.com>
2023-05-28 16:43:59 +0700

    feat: add products data into product list view

--- app/controllers/Product.php
+++ app/controllers/Product.php
@@ -4,6 +4,15 @@ class Product extends Controller {
   public function index()
   {
     $data['judul'] = 'Product List | Dermanifest';
+    $data['product_list'] = $this->model('Product_model')->getAllProduct();
+    $data['category_list'] = $this->model('Category_model')->getAllCategory();
+    $product_category_names = [];
+
+    foreach ($data['category_list'] as $category) {
+      $product_category_names[$category['id_category']] = $category['name'];
+    }
+  
+    $data['category_names'] = $product_category_names;
 
     $this->view('layout/header', $data);
     $this->view('layout/navbar');
