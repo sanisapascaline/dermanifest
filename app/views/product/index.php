b678b6d9ddb19beca711f7b49e402ef7e6703438
sanisapascaline <58965231+sanisapascaline@users.noreply.github.com>
2023-05-28 15:02:54 +0700

    feat: add UI code for product list

---
+++ app/views/product/index.php
@@ -0,0 +1,287 @@
+<section class="product container container-product">
+  <h1>All Product</h1>
+  <div class="swiper productSwiper">
+    <div class="row row-title-product mb-5">
+      <div class="col-lg-12 col-md-12 col-sm-12">
+        <div class="product-content">
+          <div class="title">
+            <h3>Dermanifest Etawa Goat Milk Mask</h3>
+          </div>
+          <div class="paragraph">
+            <p>Clean and recharge your face with our Etawa Goat Milk Mask.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="row row-product swiper-wrapper mb-5" id="scrollhere">
+      <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 swiper-slide">
+        <a href="#">
+          <div class="card swiper-slide">
+            <div class="image-product">
+              <div class="hover-desc">
+                <p><i class="fa fa-search" aria-hidden="true"></i></p>
+              </div>
+              <img src="https://dummyimage.com/400x600/000/fff" alt="...">
+            </div>
+
+            <div class="product-content">
+              <div class="text-content">
+                <h5>Product Name 1</h5>
+                <p>Product Category 1</p>
+              </div>
+
+              <div class="price-rate">
+                <div class="product-price">
+                  <p>Rp30.000,-</p>
+                </div>
+              </div>
+
+              <a href="#" class="btn btn-buy btn-cart">
+                <box-icon type='solid' name='cart-add'><i class='bx bxs-cart-add' style="width: 20px; height: auto;"></i></box-icon> Add to Cart 
+              </a>
+            </div>
+          </div>
+        </a>
+      </div>
+      
+      <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 swiper-slide">
+        <a href="#">
+          <div class="card swiper-slide">
+            <div class="image-product">
+              <div class="hover-desc">
+                <p><i class="fa fa-search" aria-hidden="true"></i></p>
+              </div>
+              <img src="https://dummyimage.com/400x600/000/EE4B2B" alt="...">
+            </div>
+
+            <div class="product-content">
+              <div class="text-content">
+                <h5>Product Name 2</h5>
+                <p>Product Category 2</p>
+              </div>
+
+              <div class="price-rate">
+                <div class="product-price">
+                  <p>Rp30.000,-</p>
+                </div>
+              </div>
+
+              <a href="#" class="btn btn-buy btn-cart">
+                <box-icon type='solid' name='cart-add'><i class='bx bxs-cart-add' style="width: 20px; height: auto;"></i></box-icon> Add to Cart 
+              </a>
+            </div>
+          </div>
+        </a>
+      </div>
+      
+      <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 swiper-slide">
+        <a href="#">
+          <div class="card swiper-slide">
+            <div class="image-product">
+              <div class="hover-desc">
+                <p><i class="fa fa-search" aria-hidden="true"></i></p>
+              </div>
+              <img src="https://dummyimage.com/400x600/000/00f2e9" alt="...">
+            </div>
+
+            <div class="product-content">
+              <div class="text-content">
+                <h5>Product Name 3</h5>
+                <p>Product Category 3</p>
+              </div>
+
+              <div class="price-rate">
+                <div class="product-price">
+                  <p>Rp30.000,-</p>
+                </div>
+              </div>
+
+              <a href="#" class="btn btn-buy btn-cart">
+                <box-icon type='solid' name='cart-add'><i class='bx bxs-cart-add' style="width: 20px; height: auto;"></i></box-icon> Add to Cart 
+              </a>
+            </div>
+          </div>
+        </a>
+      </div>
+    </div> 
+    <div class="row">
+      <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 d-flex col-button">
+        <p class="button-prev me-3">PREV</p>
+        <p class="button-next">NEXT</p>
+      </div>
+    </div>
+  </div>
+
+  <div class="swiper productSwiper">
+    <div class="row row-title-product mb-5">
+      <div class="col-lg-12 col-md-12 col-sm-12">
+        <div class="product-content">
+          <div class="title">
+            <h3>Dermanifest Aromatherapy Candle</h3>
+          </div>
+          <div class="paragraph">
+            <p>Manifest your perfect inner tranquilty with our Aromatherapy Candle series.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="row row-product swiper-wrapper mb-5" id="scrollhere">
+      <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 swiper-slide">
+        <a href="#">
+          <div class="card swiper-slide">
+            <div class="image-product">
+              <div class="hover-desc">
+                <p><i class="fa fa-search" aria-hidden="true"></i></p>
+              </div>
+              <img src="https://dummyimage.com/400x600/000/fff" alt="...">
+            </div>
+
+            <div class="product-content">
+              <div class="text-content">
+                <h5>Product Name 1</h5>
+                <p>Product Category 1</p>
+              </div>
+
+              <div class="price-rate">
+                <div class="product-price">
+                  <p>Rp30.000,-</p>
+                </div>
+              </div>
+
+              <a href="#" class="btn btn-buy btn-cart">
+                <box-icon type='solid' name='cart-add'><i class='bx bxs-cart-add' style="width: 20px; height: auto;"></i></box-icon> Add to Cart 
+              </a>
+            </div>
+          </div>
+        </a>
+      </div>
+      
+      <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 swiper-slide">
+        <a href="#">
+          <div class="card swiper-slide">
+            <div class="image-product">
+              <div class="hover-desc">
+                <p><i class="fa fa-search" aria-hidden="true"></i></p>
+              </div>
+              <img src="https://dummyimage.com/400x600/000/EE4B2B" alt="...">
+            </div>
+
+            <div class="product-content">
+              <div class="text-content">
+                <h5>Product Name 2</h5>
+                <p>Product Category 2</p>
+              </div>
+
+              <div class="price-rate">
+                <div class="product-price">
+                  <p>Rp30.000,-</p>
+                </div>
+              </div>
+
+              <a href="#" class="btn btn-buy btn-cart">
+                <box-icon type='solid' name='cart-add'><i class='bx bxs-cart-add' style="width: 20px; height: auto;"></i></box-icon> Add to Cart 
+              </a>
+            </div>
+          </div>
+        </a>
+      </div>
+      
+      <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 swiper-slide">
+        <a href="#">
+          <div class="card swiper-slide">
+            <div class="image-product">
+              <div class="hover-desc">
+                <p><i class="fa fa-search" aria-hidden="true"></i></p>
+              </div>
+              <img src="https://dummyimage.com/400x600/000/00f2e9" alt="...">
+            </div>
+
+            <div class="product-content">
+              <div class="text-content">
+                <h5>Product Name 3</h5>
+                <p>Product Category 3</p>
+              </div>
+
+              <div class="price-rate">
+                <div class="product-price">
+                  <p>Rp30.000,-</p>
+                </div>
+              </div>
+
+              <a href="#" class="btn btn-buy btn-cart">
+                <box-icon type='solid' name='cart-add'><i class='bx bxs-cart-add' style="width: 20px; height: auto;"></i></box-icon> Add to Cart 
+              </a>
+            </div>
+          </div>
+        </a>
+      </div>
+      
+      <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 swiper-slide">
+        <a href="#">
+          <div class="card swiper-slide">
+            <div class="image-product">
+              <div class="hover-desc">
+                <p><i class="fa fa-search" aria-hidden="true"></i></p>
+              </div>
+              <img src="https://dummyimage.com/400x600/000/FFFF00" alt="...">
+            </div>
+
+            <div class="product-content">
+              <div class="text-content">
+                <h5>Product Name 4</h5>
+                <p>Product Category 4</p>
+              </div>
+
+              <div class="price-rate">
+                <div class="product-price">
+                  <p>Rp30.000,-</p>
+                </div>
+              </div>
+
+              <a href="#" class="btn btn-buy btn-cart">
+                <box-icon type='solid' name='cart-add'><i class='bx bxs-cart-add' style="width: 20px; height: auto;"></i></box-icon> Add to Cart 
+              </a>
+            </div>
+          </div>
+        </a>
+      </div>
+
+      <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 swiper-slide">
+        <a href="#">
+          <div class="card swiper-slide">
+            <div class="image-product">
+              <div class="hover-desc">
+                <p><i class="fa fa-search" aria-hidden="true"></i></p>
+              </div>
+              <img src="https://dummyimage.com/400x600/000/0df200" alt="...">
+            </div>
+
+            <div class="product-content">
+              <div class="text-content">
+                <h5>Product Name 5</h5>
+                <p>Product Category 5</p>
+              </div>
+
+              <div class="price-rate">
+                <div class="product-price">
+                  <p>Rp30.000,-</p>
+                </div>
+              </div>
+
+              <a href="#" class="btn btn-buy btn-cart">
+                <box-icon type='solid' name='cart-add'><i class='bx bxs-cart-add' style="width: 20px; height: auto;"></i></box-icon> Add to Cart 
+              </a>
+            </div>
+          </div>
+        </a>
+      </div>
+
+    </div> 
+    <div class="row">
+      <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 d-flex col-button">
+        <p class="button-prev me-3">PREV</p>
+        <p class="button-next">NEXT</p>
+      </div>
+    </div>
+  </div>
+</section> 
