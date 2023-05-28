5d9ad0db915c781219f7106c5e0d04cfd388b21e
sanisapascaline <58965231+sanisapascaline@users.noreply.github.com>
2023-05-29 00:12:14 +0700

    feat: add UI code for product detail

---
+++ app/views/product/detail.php
@@ -0,0 +1,88 @@
+<section class="product-detail container mb-5">
+  <div class="row mt-3">
+    
+    <div class="pictures d-flex justify-content-center col-md-6">
+      <div class="">
+        <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="true">
+        <div class="carousel-indicators">
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="3" aria-label="Slide 3"></button>
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="4" aria-label="Slide 3"></button>
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="5" aria-label="Slide 3"></button>
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="6" aria-label="Slide 3"></button>
+          </div>
+          <div class="carousel-inner">
+            <div class="carousel-item active">
+              <img src="https://dummyimage.com/400x600/000/fff" class="d-block w-100" alt="...">
+            </div>
+            <div class="carousel-item">
+              <img src="https://dummyimage.com/600x400/000/fff" class="d-block w-100" alt="...">
+            </div>
+            <div class="carousel-item">
+              <img src="https://dummyimage.com/400x600/000/fff" class="d-block w-100" alt="...">
+            </div>
+            <div class="carousel-item">
+              <img src="https://dummyimage.com/400x600/000/fff" class="d-block w-100" alt="...">
+            </div>
+            <div class="carousel-item">
+              <img src="https://dummyimage.com/400x600/000/fff" class="d-block w-100" alt="...">
+            </div>
+            <div class="carousel-item">
+              <img src="https://dummyimage.com/400x600/000/fff" class="d-block w-100" alt="...">
+            </div>
+            <div class="carousel-item">
+              <img src="https://dummyimage.com/400x600/000/fff" class="d-block w-100" alt="...">
+            </div>
+          </div>
+          <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+          </button>
+          <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+          </button>
+        </div>  
+        
+      </div>
+    </div>
+
+    <div class="details col-md-6">
+      <nav aria-label="breadcrumb">
+        <ol class="breadcrumb">
+          <li class="breadcrumb-item"><a href="<?= BASEURL; ?>"><u>Home</a></u></li>
+          <li class="breadcrumb-item"><a href="<?= BASEURL; ?>/product"><u>Product List</a></u></li>
+          <li class="breadcrumb-item active" aria-current="page">Product Name</li>
+        </ol>
+      </nav>
+      <h2>Product Name</h2>
+      <div class="description-detail mt-3">
+        <h4>Description</h4>
+        <p>Lorem ipsum</p>
+      </div>
+      <div class="instruction-detail mt-3">
+        <h4>How to Use</h4>
+        <p>Lorem ipsum</p>
+      </div>
+      <div class="ingredients-detail mt-3">
+        <h4>Ingredients</h4>
+        <p>Lorem ipsum</p>
+      </div>
+      <div class="neto-price d-flex justify-content-between">
+        <div class="neto-detail mt-3">
+          <h4>Neto</h4>
+          <p>30 gram</p>
+        </div>
+        <div class="price-detail mt-3">
+          <h4>Price</h4>
+          <h3>Rp35.000,-</h3>
+        </div>    
+      </div>
+      <div class="d-flex mt-4">
+        <a href="#" class="btn btn-buy w-100"><i class="fas fa-cart-plus"></i>Add to Cart</a>
+      </div>
+    </div>
+  </div>
+</section>
