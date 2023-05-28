01c3f7d72aff07632a01f7f175e05ec5fd2eaa95
anindyoo <anindyo0216@gmail.com>
2023-05-28 19:01:58 +0700

    feat: improve products data structure for product list view

--- app/controllers/Product.php
+++ app/controllers/Product.php
@@ -6,14 +6,21 @@ class Product extends Controller {
     $data['judul'] = 'Product List | Dermanifest';
     $data['product_list'] = $this->model('Product_model')->getAllProduct();
     $data['category_list'] = $this->model('Category_model')->getAllCategory();
-    $product_category_names = [];
 
-    foreach ($data['category_list'] as $category) {
-      $product_category_names[$category['id_category']] = $category['name'];
+    foreach ($data['category_list'] as $cat_key => $category) {
+      $prod_match_category = [];
+      foreach ($data['product_list'] as $product) {
+        if ($category['id_category'] == $product['id_category']) {
+          array_push($prod_match_category, $product);
+        }
+      }
+      $category_with_product[$cat_key] = [
+        ...$category, 
+        'category_product' => $prod_match_category
+      ];
     }
-  
-    $data['category_names'] = $product_category_names;
 
+    $data['categorized_product'] = $category_with_product;
     $this->view('layout/header', $data);
     $this->view('layout/navbar');
     $this->view('product/index', $data);
