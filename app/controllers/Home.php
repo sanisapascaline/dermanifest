3cadac1695f9d3928cec0876ff8f1e7a84871341
anindyoo <anindyo0216@gmail.com>
2023-05-29 18:37:02 +0700

    feat: add products data into home view

--- app/controllers/Home.php
+++ app/controllers/Home.php
@@ -4,6 +4,7 @@ class Home extends Controller {
   public function index() 
   {
     $data['judul'] = 'Home | Dermanifest';
+    $data['product_list'] = $this->model('Product_model')->getAllProduct();
     
     $this->view('layout/header', $data);
     $this->view('layout/navbar');
