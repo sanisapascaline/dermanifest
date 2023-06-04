ab2b078a719a188a1035d574098456de9566180d
sanisapascaline <58965231+sanisapascaline@users.noreply.github.com>
2023-06-04 21:52:55 +0800

    fix: fix scripts

--- app/views/layout/footer.php
+++ app/views/layout/footer.php
@@ -1,11 +1,11 @@
 <!-- Swiper JS -->
-<script src="https://unpkg.com/swiper@7/swiper-bundle.min.js"></script
-<!-- Local JS -->
-<script src="<?= BASEURL; ?>/assets/js/main.js"></script>
+<script src="https://unpkg.com/swiper@7/swiper-bundle.min.js"></script>
 <!-- Jquery -->
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 <!-- Bootstrap 5 -->
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>
+<!-- Local JS -->
+<script src="<?= BASEURL; ?>/assets/js/main.js"></script>
 </body>
 </html>
