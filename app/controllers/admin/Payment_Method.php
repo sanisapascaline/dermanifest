251564fdddcbff42ecf3ad1598dc97a7b10e505f
anindyoo <anindyo0216@gmail.com>
2023-06-06 16:29:21 +0700

    feat: add method for inserting new payment method

--- app/controllers/admin/Payment_Method.php
+++ app/controllers/admin/Payment_Method.php
@@ -28,5 +28,17 @@ class Payment_Method extends Controller {
     $this->viewAdmin('payment_method/add', $data);
     $this->view('layout/admin/footer');
   }
+  
+  public function insert() 
+  {
+    if ($this->model('Payment_Method_model')->insertDataPaymentMethod($_POST) > 0) {
+      Flasher::setFlash('Success.', 'Payment Method: <strong>' . $_POST['payment_service'] . '</strong> has been added.', 'success');
+      header('Location:' . ADMINURL .'/payment_method');
+    } else {
+      Flasher::setFlash('Error.', 'Failed to add Payment Method.', 'danger');
+      header('Location: ' . ADMINURL . '/payment_method/add');
+      exit;
+    }
+  }
 }
 ?>
