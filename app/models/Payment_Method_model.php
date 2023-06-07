dd57a733b9ac6cd9b222b34337be0e7f57cf3356
anindyoo <anindyo0216@gmail.com>
2023-06-07 17:41:37 +0700

    feat: add query for updating payment method

--- app/models/Payment_Method_model.php
+++ app/models/Payment_Method_model.php
@@ -36,5 +36,19 @@ class Payment_Method_model {
 
     return $this->db->rowCount();
   }
+
+  public function updateDataPaymentMethod($data)
+  {
+    $query = "UPDATE " . $this->table . " SET payment_service = :payment_service, account_number = :account_number, account_name = :account_name WHERE id_payment_method = :id_payment_method";
+		$this->db->query($query);
+		$this->db->bind('id_payment_method', $data['id_payment_method']);
+		$this->db->bind('payment_service', $data['payment_service']);
+    $this->db->bind('account_number', $data['account_number']);
+    $this->db->bind('account_name', $data['account_name']);
+		
+    $this->db->execute();
+
+		return $this->db->rowCount();
+  }
 }
 ?>
